687075b426e211908f2c22798357aa9e9273cbdc
alegresor <agsorokin3@gmail.com>
2026-06-12 07:55:50 -0500

    both notebooks running fine

--- rmatqmcpy/__init__.py
+++ rmatqmcpy/__init__.py
@@ -38,5 +38,4 @@ from .kernels import (
 
 from .opt import (
     opt_weights_sum_1,
-    wce_squared_plus_kernel_integral,
 )
--- rmatqmcpy/opt.py
+++ rmatqmcpy/opt.py
@@ -3,7 +3,7 @@ import torch
 from .kernels import KernelMatern
 from .rand_Stiefel_Gr_flag_LGr import rand_flag_R
 
-def opt_weights_sum_1(x, kernel, dims=2, eps=1e-12):
+def opt_weights_sum_1(x, kernel, dims=2, eps=1e-12, return_wce2_p_kmean=True):
     r"""
     Compute the optimal weights for a kernel quadrature rule.
 
@@ -12,6 +12,10 @@ def opt_weights_sum_1(x, kernel, dims=2, eps=1e-12):
         kernel (callable): Kernel function.
         dims (int): See description of `x`. 
         eps (float, optional): Regularization parameter for numerical stability.
+        return_wce2_p_kmean (bool): If True, return 
+            $\mathrm{WCE}^2+\gamma = \boldsymbol{w}^\intercal \mathsf{K} \boldsymbol{w}$ 
+            where $\gamma = \int_\mathcal{M} K(\boldsymbol{x},\boldsymbol{y}) \mu(\mathrm{d} \boldsymbol{x})$ 
+            is assumed to be constant as a function of $\boldsymbol{x}$. 
 
     Returns:
         w (torch.Tensor): Optimal weights of shape `(..., N)`.
@@ -23,85 +27,41 @@ def opt_weights_sum_1(x, kernel, dims=2, eps=1e-12):
         >>> x = rand_flag_R(3,7,seed=7)
         >>> x.shape
         torch.Size([3, 7, 7])
-        >>> w = opt_weights_sum_1(x,kernel)
+        >>> w,wce2_p_kmean = opt_weights_sum_1(x,kernel)
         >>> w.shape 
         torch.Size([3])
         >>> w.sum() 
         tensor(1.)
         >>> w
         tensor([0.3506, 0.3218, 0.3276])
-
-        >>> x2 = rand_flag_R(3,7,seed=11)
-        >>> xfull = torch.stack([x,x2],dim=0)
-        >>> xfull.shape 
-        torch.Size([2, 3, 7, 7])
-        >>> wfull = opt_weights_sum_1(xfull,kernel)
-        >>> wfull.shape 
-        torch.Size([2, 3])
-        >>> wfull.sum(-1) 
-        tensor([1., 1.])
-        >>> wfull
-        tensor([[0.3506, 0.3218, 0.3276],
-                [0.3015, 0.3055, 0.3931]])
-    """
-    assert x.ndim>=(dims+1)
-    kmat = kernel(x.unsqueeze(-dims-1),x.unsqueeze(-dims-2),dim=tuple(j for j in range(-dims,0,1))) # (...,N,N)
-    b = torch.ones(kmat.shape[:-1],device=x.device) # (...,N)
-    L = torch.linalg.cholesky(kmat+eps*torch.eye(kmat.size(-1),device=x.device),upper=False) # (...,N,N)
-    w = torch.cholesky_solve(b[...,None],L,upper=False)[...,0] # (...,N)
-    return w/w.sum(-1,keepdim=True)
-
-def wce_squared_plus_kernel_integral(x, kernel, w, dims=2):
-    r"""
-    Square of the worst case error (WCE) plus the integral value, 
-    
-    $$\mathrm{WCE}^2+\gamma = \boldsymbol{w}^\intercal \mathsf{K} \boldsymbol{w}$$
-    
-    where $\gamma = \int_\mathcal{M} K(\boldsymbol{x},\boldsymbol{y}) \mu(\mathrm{d} \boldsymbol{x})$ is assumed to be constant as a function of $\boldsymbol{x}$. 
-
-    Args:
-        x (torch.Tensor): A batch of samples of shape `(..., N, *m)` where `len(m)==dims`.
-        w (torch.Tensor): A torch.tensor of shape `(..., N)`
-        kernel (callable): Kernel function.
-        dims (int): See description of `x`. 
-
-    Returns:
-        val (torch.Tensor): Squree WCE plus kernel integral value.
-    
-    Examples:
-        >>> torch.set_default_dtype(torch.float64) 
-
-        >>> kernel = KernelMatern(nu=1/2)
-        >>> x = rand_flag_R(3,7,seed=7)
-        >>> x.shape
-        torch.Size([3, 7, 7])
-        >>> w = opt_weights_sum_1(x,kernel)
-        >>> w.shape 
-        torch.Size([3])
-        >>> w.sum() 
-        tensor(1.)
-        >>> w
-        tensor([0.3506, 0.3218, 0.3276])
-        >>> 
-        >>> wce_squared_plus_kernel_integral(x,kernel,w)
+        >>> wce2_p_kmean
         tensor(0.7027)
 
         >>> x2 = rand_flag_R(3,7,seed=11)
         >>> xfull = torch.stack([x,x2],dim=0)
         >>> xfull.shape 
         torch.Size([2, 3, 7, 7])
-        >>> wfull = opt_weights_sum_1(xfull,kernel)
-        >>> wfull.shape 
+        >>> w_full,wce2_p_kmean_full = opt_weights_sum_1(xfull,kernel)
+        >>> w_full.shape 
         torch.Size([2, 3])
-        >>> wfull.sum(-1) 
+        >>> w_full.sum(-1) 
         tensor([1., 1.])
-        >>> wfull
+        >>> w_full
         tensor([[0.3506, 0.3218, 0.3276],
                 [0.3015, 0.3055, 0.3931]])
-        >>> wce_squared_plus_kernel_integral(xfull,kernel,wfull)
+        >>> wce2_p_kmean_full.shape 
+        torch.Size([2])
+        >>> wce2_p_kmean_full
         tensor([0.7027, 0.7009])
     """
     assert x.ndim>=(dims+1)
-    assert w.shape==x.shape[:-dims]
     kmat = kernel(x.unsqueeze(-dims-1),x.unsqueeze(-dims-2),dim=tuple(j for j in range(-dims,0,1))) # (...,N,N)
-    return torch.einsum("...i,...ij,...j->...",w,kmat,w) # (...,)
+    b = torch.ones(kmat.shape[:-1],device=x.device) # (...,N)
+    L = torch.linalg.cholesky(kmat+eps*torch.eye(kmat.size(-1),device=x.device),upper=False) # (...,N,N)
+    w = torch.cholesky_solve(b[...,None],L,upper=False)[...,0] # (...,N)
+    w = w/w.sum(-1,keepdim=True)
+    if return_wce2_p_kmean:
+        wce_squared_plus_kernel_integral = torch.einsum("...i,...ij,...j->...",w,kmat,w) # (...,)
+        return w,wce_squared_plus_kernel_integral
+    else:
+        return w
